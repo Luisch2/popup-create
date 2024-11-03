2603f82181385982f7ed0b1c48ac4993abc79bde
Luisch2 <camposluis2722@gmail.com>
2024-11-03 15:25:09 -0500

    Udpate

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [showPopup, setShowPopup] = useState(false);
-  const [data, setData] = useState(null);
+  const [personajes, setPersonajes] = useState(null); // Inicializa en null para manejar el objeto completo
 
-  // Función para hacer la solicitud con Axios
-  const fetchReferido = () => {
-    axios.get("/generate_nro_referido")
-      .then((response) => setData(response.data)) // Guardamos los datos recibidos
-      .catch((error) => console.error("Error al obtener datos:", error));
-  };
+  useEffect(() => {
+    async function fetchPersonaje() {
+      try {
+        const response = await fetch("http://localhost:3000/generate_nro_referido/update_code");
+        const data = await response.json();
+        setPersonajes(data); // Asigna el objeto completo directamente
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      }
+    }
+
+    if (showPopup) {
+      fetchPersonaje();
+    }
+  }, [showPopup]);
 
   const togglePopup = () => {
     setShowPopup(!showPopup);
-    if (!showPopup) {
-      // Solo hacemos la solicitud cuando se abre el popup
-      fetchReferido();
-    }
   };
 
   return (
-    <div className="App">
+    <div>
       <button onClick={togglePopup} className="bg-blue-500 text-white px-4 py-2 rounded">
-        Mostrar Referido
+        Mostrar Personaje
       </button>
 
-      {showPopup && data && (
+      {showPopup && personajes && (
         <div className="popup bg-white shadow-lg p-6 rounded-lg fixed inset-0 flex items-center justify-center">
-          <div className="bg-white p-6 rounded-lg shadow-xl">
-            <h2 className="text-xl font-bold">
-              Hola {data.nombre}, aquí tienes tu link de referido:
-            </h2>
+          <div className="bg-white p-6 rounded-lg shadow-xl max-h-screen overflow-y-auto">
+            <h2 className="text-xl font-bold mb-4">Hola, {personajes.name}, tu número de referido es:</h2>
             <p>
               <a
-                href={`https://fesosa.com/Laia/${data.id}`}
+                href={`https://fesosa.com/Laia/${personajes.refer_code}`}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-500 underline"
               >
-                https://fesosa.com/Laia/{data.id}
+                https://fesosa.com/Laia/{personajes.refer_code}
               </a>
             </p>
             <button onClick={togglePopup} className="bg-red-500 text-white px-4 py-2 mt-4 rounded">
